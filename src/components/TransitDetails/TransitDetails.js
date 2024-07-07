2778f8bfa13217fcd3503fbb4a02e52585c63fd1
Justin Soon <jienkhye01@gmail.com>
2024-07-08 04:00:44 +0800

    kinda edited the transitdetails cards abit

--- src/components/TransitDetails/TransitDetails.js
+++ src/components/TransitDetails/TransitDetails.js
@@ -1,82 +1,92 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
 
 const TransitDetails = ({ details }) => {
     if (!details || details.length === 0) {
         return <View><Text>No transit details available.</Text></View>;
     }
-    
+
     return (
         <ScrollView style={styles.container}>
-            {details.map((leg, index) => {
-                // console.log(leg);
-                return (
-                    <View key={index} style={styles.legContainer}>
-                        <Text style={styles.legTitle}>Leg {index + 1}</Text>
-                        <Text style={styles.distance}>Distance: {leg.distance || 'Not available'}</Text>
-                        {leg.steps.map((step, stepIndex) => {
-                            const modifiedInstructions = step.instructions.replace('Subway', 'MRT');
-                            const vehicle = step.transit_details?.vehicle?.type;
-                            let detailedInstructions = modifiedInstructions;
+            {details.map((leg, index) => (
+                <Card key={index} containerStyle={styles.legContainer}>
+                    <Card.Title style={styles.legTitle}>Leg {index + 1}</Card.Title>
+                    <Card.Divider/>
+                    <Text style={styles.distance}>
+                        <Icon name="map-marker-distance" type="material-community" color="#517fa4" /> Distance: {leg.distance || 'Not available'}
+                    </Text>
+                    {leg.steps.map((step, stepIndex) => {
+                        const modifiedInstructions = step.instructions.replace('Subway', 'MRT');
+                        const vehicle = step.transit_details?.vehicle?.type;
+                        let detailedInstructions = modifiedInstructions;
 
-                            if (vehicle === 'BUS' && step.transit_details?.line?.short_name) {
-                                detailedInstructions += ` (Bus numbers: ${step.transit_details.line.short_name})`;
-                            }
-                            console.log("Detailed Instructions for step", stepIndex, ":", detailedInstructions);
-
-                            return (
-                                <View key={stepIndex} style={styles.stepContainer}>
-                                    <Text style={styles.instructions}>{detailedInstructions}</Text>
-                                    {step.transit_details && (
-                                        <View style={styles.transitDetailsContainer}>
-                                            <Text style={styles.transitText}>Depart from: {step.transit_details.departure_stop}</Text>
-                                            <Text style={styles.transitText}>Arrive at: {step.transit_details.arrival_stop}</Text>
-                                            <Text style={styles.transitText}>Line: {step.transit_details.line}</Text>
-                                            <Text style={styles.transitText}>Vehicle: {step.transit_details.vehicle}</Text>
-                                        </View>
-                                    )}
-                                </View>
-                            );
-                        })}
-                    </View>
-                );
-            })}
+                        if (vehicle === 'BUS' && step.transit_details?.line?.short_name) {
+                            detailedInstructions += ` (Bus numbers: ${step.transit_details.line.short_name})`;
+                        }
+                        console.log("i want ", step.transit_details);
+                        return (
+                            <View key={stepIndex} style={styles.stepContainer}>
+                                <Text style={styles.instructions}>
+                                    <Icon name="walk" type="material-community" color="#517fa4" /> {detailedInstructions}
+                                </Text>
+                                {step.transit_details && (
+                                    <View style={styles.transitDetailsContainer}>
+                                        {step.transit_details.departure_stop && (
+                                            <Text style={styles.transitText}>
+                                                <Icon name="departure-board" type="material-community" color="#517fa4" /> Depart from: {step.transit_details.departure_stop}
+                                            </Text>
+                                        )}
+                                        {step.transit_details.arrival_stop && (
+                                            <Text style={styles.transitText}>
+                                                <Icon name="flag-checkered" type="material-community" color="#517fa4" /> Arrive at: {step.transit_details.arrival_stop}
+                                            </Text>
+                                        )}
+                                        {step.transit_details.line && (
+                                            <Text style={styles.transitText}>
+                                                <Icon name="bus" type="material-community" color="#517fa4" /> Line: {step.transit_details.line}
+                                            </Text>
+                                        )}
+                                        {step.transit_details.vehicle && (
+                                            <Text style={styles.transitText}>
+                                                <Icon name="train" type="material-community" color="#517fa4" /> Vehicle: {step.transit_details.vehicle}
+                                            </Text>
+                                        )}
+                                    </View>
+                                )}
+                            </View>
+                        );
+                    })}
+                </Card>
+            ))}
         </ScrollView>
     );
 };
-
-
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingHorizontal: 10,
+        backgroundColor: '#f0f0f0',
     },
     legContainer: {
-        marginBottom: 20,
-        padding: 10,
-        backgroundColor: '#FFFFFF',
         borderRadius: 10,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
+        backgroundColor: '#fff',
+        marginBottom: 20,
+        elevation: 3,
     },
     legTitle: {
         fontSize: 18,
         fontWeight: 'bold',
-        marginBottom: 5,
+        color: '#333',
     },
     stepContainer: {
-        marginBottom: 5,
+        marginVertical: 5,
     },
     instructions: {
         fontSize: 16,
         color: '#333',
+        marginVertical: 5,
     },
     transitDetailsContainer: {
         marginTop: 5,
@@ -87,12 +97,15 @@ const styles = StyleSheet.create({
     transitText: {
         fontSize: 14,
         color: '#555',
+        marginVertical: 2,
     },
     distance: {
         fontSize: 16,
         color: '#333',
-        marginBottom: 10
-    }
+        marginBottom: 10,
+        display: 'flex',
+        alignItems: 'center',
+    },
 });
 
 export default TransitDetails;
